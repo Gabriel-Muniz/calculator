9a16138652be6dcf214f6cbd13abade4240578ff
Gabriel Muniz <56240854+Gabriel-Muniz@users.noreply.github.com>
2023-10-09 20:14:08 -0300

    feat add operations functions

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+const add = (num1 ,num2) => num1 + num2;
+const subtract = (num1, num2) => num1 - num2;
+const multiply = (num1, num2) => num1 * num2;
+const divide = (num1, num2) => num1/num2;
+
+let num1 = 0;
+let num2 = 0;
+let operator;
