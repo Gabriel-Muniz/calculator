868fb0872e658425069c88c9b733d774930252b6
Gabriel Muniz <56240854+Gabriel-Muniz@users.noreply.github.com>
2024-09-19 19:53:53 -0300

    Feat: add operate function

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 const addCalc = (n1, n2) => n1 + n2;
-const subCalc = (n1, n2) => n1 - n2;
+const subtractCalc = (n1, n2) => n1 - n2;
 const multiplyCalc = (n1, n2) => n1 * n2;
 const divideCalc = (n1, n2) => n1 / n2;
 
-console.log(
-  `
-    Add: ${addCalc(15, 3)}
-    Subtract: ${subCalc(15, 3)}
-    Multiply: ${multiplyCalc(15, 3)}
-    Divide: ${divideCalc(15, 3)}
-  `);
+const number1 = 1;
+const number2 = 2;
+
+function operate(operator, n1, n2) {
+  switch (operator) {
+    case "add":
+      console.log(addCalc(n1, n2));
+      break;
+    case "subtract":
+      console.log(subtractCalc(n1, n2));
+      break;
+    case "multiply":
+      console.log(multiplyCalc(n1, n2));
+      break;
+    case "divide":
+      console.log(divideCalc(n1, n2));
+      break;
+
+    default:
+      break;
+  }
+}
