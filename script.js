269d9f586144a69c0a5417d5388b351230ab2f2b
Gabriel Muniz <56240854+Gabriel-Muniz@users.noreply.github.com>
2024-09-22 13:54:30 -0300

    Feat!: finish basic operations logic

--- script.js
+++ script.js
@@ -5,21 +5,29 @@ const divideCalc = (n1, n2) => n1 / n2;
 
 let number1 = null;
 let number2 = null;
+let operation = "";
 
 function operate(operator, n1, n2) {
   switch (operator) {
     case "add":
-      console.log(addCalc(n1, n2));
-      break;
+      console.log(operator);
+
+      return addCalc(n1, n2);
+
     case "subtract":
-      console.log(subtractCalc(n1, n2));
-      break;
+      console.log(operator);
+
+      return subtractCalc(n1, n2);
+
     case "multiply":
-      console.log(multiplyCalc(n1, n2));
-      break;
+      console.log(operator);
+
+      return multiplyCalc(n1, n2);
+
     case "divide":
-      console.log(divideCalc(n1, n2));
-      break;
+      console.log(operator);
+
+      return divideCalc(n1, n2);
 
     default:
       break;
@@ -27,7 +35,7 @@ function operate(operator, n1, n2) {
 }
 
 const getOperation = function (operation) {
-  let operationSymbol = '';
+  let operationSymbol = "";
   switch (operation) {
     case "add":
       operationSymbol = "+";
@@ -53,53 +61,84 @@ const numBtns = document.querySelectorAll(".num-btn");
 const operationBtns = document.querySelectorAll(".operation");
 const clearBtn = document.querySelector(".function-btn[value='clear']");
 const ereaseBtn = document.querySelector(".function-btn[value='erase']");
+const equalBtn = document.querySelector(".equal");
 
-const populateDisplay = function(string, concatString = true) {
-  if(concatString == true){
+const populateDisplay = function (string, concatString = true) {
+  if (concatString == true) {
     displayText.textContent += string;
     return;
-  } 
+  }
   displayText.textContent = string;
-}
+};
 
-const eraseDisplay = function() {
-  
+const eraseDisplay = function () {
   let displayValue = displayText.textContent;
-  let newDisplayValue = displayValue.slice(0, displayValue.length-1);
-  
-  if (newDisplayValue == '') {
+  let newDisplayValue = displayValue.slice(0, displayValue.length - 1);
+
+  if (newDisplayValue == "") {
     return 0;
   }
   return newDisplayValue;
-}
+};
 
-const getDisplayValue = function() {
-  return Number(displayText.textContent);
-}
+const getDisplayValue = function () {
+  let displayValue = displayText.textContent;
+  if (
+    displayValue == "+" ||
+    displayValue == "-" ||
+    displayValue == "x" ||
+    displayValue == "/"
+  ) {
+    return displayValue;
+  }
+  return Number(displayValue);
+};
 
 operationBtns.forEach((btn) => {
   btn.addEventListener("click", () => {
-    populateDisplay(getOperation(btn.value), false);
+    if (number1 == null) number1 = getDisplayValue();
+    operation = btn.value;
+
+    populateDisplay(getOperation(operation), false);
   });
 });
 
 numBtns.forEach((btn) => {
   btn.addEventListener("click", () => {
+    if (typeof getDisplayValue() != "number") {
+      populateDisplay(btn.value, false);
+      return;
+    }
     populateDisplay(btn.value);
   });
 });
 
 //Adicionar EventListener nos botões de exclusão e de igual
-clearBtn.addEventListener('click', () => {
-  populateDisplay(0, false)
-})
 
-ereaseBtn.addEventListener('click', () => {
+const resetCalc = function () {
+  number1 = number2 = null;
+  operation = "";
+  populateDisplay(0, false);
+};
+
+clearBtn.addEventListener("click", () => {
+  resetCalc();
+});
+
+ereaseBtn.addEventListener("click", () => {
+  if (number1 !== null && number2 !== null) {
+    resetCalc()
+  }
   populateDisplay(eraseDisplay(), false);
-})
+  number1 = getDisplayValue();
+});
 
+equalBtn.addEventListener("click", () => {
+  number2 = getDisplayValue();
+  populateDisplay(operate(operation, number1, number2), false);
+  number1 = getDisplayValue();
+});
 
 //Adicionar variáveis para a realização das operações
 
 //Limpar display após seleção de operação e seleção de número
-
