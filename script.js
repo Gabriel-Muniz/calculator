f17219362f8b580414153f23d2bc036eca7a3e68
Gabriel-Muniz <56240854+Gabriel-Muniz@users.noreply.github.com>
2026-08-13 12:33:42 -0300

    feat: add basic operations functions

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 console.log('Hello world!')
+
+const add = (num1, num2) => num1 + num2;
+const subtract = (num1, num2) => num1 - num2;
+const multiply = (num1, num2) => num1 * num2;
+const divide = (num1, num2) => num1 / num2;
