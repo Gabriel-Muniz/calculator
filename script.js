60717ab86c23609b06316c5c7b033168da77f05d
Gabriel-Muniz <56240854+Gabriel-Muniz@users.noreply.github.com>
2026-08-13 21:39:26 -0300

    refac: changes how event listeners are created
    split old eventListener in multiple for better event handling

--- script.js
+++ script.js
@@ -28,38 +28,20 @@ const operate = (operationNumber1, operationNumber2, operator) => {
     }
 }
 
-const calculatorButtons = document.querySelector('.calculator-buttons-container')
+const digitButtonsContainer = document.querySelector('.digits-container');
+const operationsContainer = document.querySelector('.operations-container');
+const equalBtn = document.querySelector('.equal-btn');
+const eraseBtn = document.querySelector('.erase-btn');
+const clearBtn = document.querySelector('.clear-btn');
 
-calculatorButtons.addEventListener('click', (e) => {
+clearBtn.addEventListener('click', (e) => {
+    clearCalc();
+})
+
+digitButtonsContainer.addEventListener('click', (e) => {
     let auxTarget = e.target;
 
     if (!auxTarget.value) return;
-
-    if (auxTarget.classList.contains('action-btn')) {
-        clearCalc();
-        updateDisplay('0');
-        return;
-    }
-
-    if (auxTarget.value === 'equal') {
-        let operationResult = operate(Number(operationNumber1), Number(operationNumber2), operator);
-
-        updateDisplay(`${operationNumber1} ${operator} ${operationNumber2} =`, true);
-        updateDisplay(operationResult)
-
-        operationNumber1 = operationResult;
-        operator = null;
-        operationNumber2 = null;
-        return;
-    };
-
-    if (auxTarget.classList.contains('operation-btn') && !auxTarget.classList.contains('equal-btn')) {
-
-        operator = auxTarget.value;
-        updateDisplay('0');
-        updateDisplay(`${operationNumber1} ${auxTarget.textContent}`, true);
-        return;
-    }
 
     if (operator === null) {
         operationNumber1 = (operationNumber1) ? operationNumber1 + auxTarget.value : auxTarget.value;
@@ -68,6 +50,30 @@ calculatorButtons.addEventListener('click', (e) => {
         operationNumber2 = (operationNumber2) ? operationNumber2 + auxTarget.value : auxTarget.value;
         updateDisplay(operationNumber2);
     }
+})
+
+operationsContainer.addEventListener('click', (e) => {
+    let auxTarget = e.target;
+    const isActionBtn = auxTarget.classList.contains('action-btn');
+
+    if (!auxTarget.value || isActionBtn) return;
+
+    operator = auxTarget.value;
+    updateDisplay('0');
+    updateDisplay(`${operationNumber1} ${auxTarget.textContent}`, true);
+})
+
+equalBtn.addEventListener('click', (e) => {
+    let operationResult = operate(Number(operationNumber1), Number(operationNumber2), operator);
+
+    updateDisplay(`${operationNumber1} ${operator} ${operationNumber2} =`, true);
+    updateDisplay(operationResult)
+
+    console.log(`${operationNumber1} ${operator} ${operationNumber2} = ${operationResult}`)
+
+    operationNumber1 = operationResult;
+    operator = null;
+    operationNumber2 = null;
 })
 
 const updateDisplay = (string, isSecondary = false, resetDisplay = false) => {
