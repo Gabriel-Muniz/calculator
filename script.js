262975e5ec5d1868a322176790811113e06760b7
Gabriel-Muniz <56240854+Gabriel-Muniz@users.noreply.github.com>
2026-08-13 23:56:49 -0300

    fix: format operation result to two decimal points

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 console.log('Hello world!')
 
-const add = (num1, num2) => num1 + num2;
-const subtract = (num1, num2) => num1 - num2;
-const multiply = (num1, num2) => num1 * num2;
-const divide = (num1, num2) => num1 / num2;
+const add = (num1, num2) => formatOutput(num1 + num2);
+const subtract = (num1, num2) => formatOutput(num1 - num2);
+const multiply = (num1, num2) => formatOutput(num1 * num2);
+const divide = (num1, num2) => formatOutput(num1 / num2);
+
+const formatOutput = (num) => {
+    return Number.isInteger(num) ? num : num.toFixed(2);
+}
 
 let operationNumber1 = 0;
 let operationNumber2 = null;
