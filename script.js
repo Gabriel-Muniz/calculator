5946ed39c8ccbd3d080557e16df8d43e72c20dda
Gabriel-Muniz <56240854+Gabriel-Muniz@users.noreply.github.com>
2026-08-13 21:07:16 -0300

    feat: add support to basic operations
    allows the user to make simple operations

--- script.js
+++ script.js
@@ -31,16 +31,66 @@ const operate = (operationNumber1, operationNumber2, operator) => {
 const calculatorButtons = document.querySelector('.calculator-buttons-container')
 
 calculatorButtons.addEventListener('click', (e) => {
-    if (!e.target.value) return;
+    let auxTarget = e.target;
+
+    if (!auxTarget.value) return;
+
+    if (auxTarget.classList.contains('action-btn')) {
+        clearCalc();
+        updateDisplay('0');
+        return;
+    }
+
+    if (auxTarget.value === 'equal') {
+        let operationResult = operate(Number(operationNumber1), Number(operationNumber2), operator);
+
+        updateDisplay(`${operationNumber1} ${operator} ${operationNumber2} =`, true);
+        updateDisplay(operationResult)
+
+        operationNumber1 = operationResult;
+        operator = null;
+        operationNumber2 = null;
+        return;
+    };
+
+    if (auxTarget.classList.contains('operation-btn') && !auxTarget.classList.contains('equal-btn')) {
+
+        operator = auxTarget.value;
+        updateDisplay('0');
+        updateDisplay(`${operationNumber1} ${auxTarget.textContent}`, true);
+        return;
+    }
 
     if (operator === null) {
-        operationNumber1 = (operationNumber1) ? operationNumber1 + e.target.value : e.target.value;
+        operationNumber1 = (operationNumber1) ? operationNumber1 + auxTarget.value : auxTarget.value;
         updateDisplay(operationNumber1);
+    } else {
+        operationNumber2 = (operationNumber2) ? operationNumber2 + auxTarget.value : auxTarget.value;
+        updateDisplay(operationNumber2);
     }
 })
 
-const updateDisplay = (string) => {
-    const outDisplay = document.querySelector('.result-display');
+const updateDisplay = (string, isSecondary = false, resetDisplay = false) => {
+    const outDisplay = document.querySelector('.main-display');
+    const outSecondaryDisplay = document.querySelector('.secondary-display');
+
+
+    if (resetDisplay) {
+        outSecondaryDisplay.textContent = '';
+    }
+
+    if (isSecondary) {
+        outSecondaryDisplay.textContent = string;
+        return;
+    }
 
     outDisplay.textContent = string;
+}
+
+const clearCalc = () => {
+    operationNumber1 = null;
+    operationNumber2 = null;
+    operator = null;
+
+    updateDisplay('0', false, true);
 }
