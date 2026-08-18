97cb4eabb44fe9b7befc3d06abcbeec4a49e4966
Gabriel-Muniz <56240854+Gabriel-Muniz@users.noreply.github.com>
2026-08-18 20:20:07 -0300

    fix: wrong object name

--- script.js
+++ script.js
@@ -129,7 +129,7 @@ equalBtn.addEventListener('click', (e) => {
     let operationResult = operate(operationValues);
     operationValues.isResult = true;
 
-    updateDisplay(`${operationValues.num1} ${operationSigns[operationValues.operator]} ${operationValues.num2} =`, true);
+    updateDisplay(`${operationValues.num1} ${OPERATION_SIGN[operationValues.operator]} ${operationValues.num2} =`, true);
     updateDisplay(operationResult)
 
     console.log(`${operationValues.num1} ${operationValues.operator} ${operationValues.num2} = ${operationResult}`)
